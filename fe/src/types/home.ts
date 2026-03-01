52fa8b7fe62a84eaacf07ea5b712b1a04f100c12
Yejun Lee <bensteve620@gmail.com>
2026-03-01 13:06:19 -0500

    Define home page types

---
+++ fe/src/types/home.ts
@@ -0,0 +1,16 @@
+export type NewsItem = {
+  id: string;
+  title: string;
+  summary: string;
+  publishedAt: string; 
+  url?: string;
+  source?: string;
+};
+
+export type TopPlayer = {
+  id: string;
+  name: string;
+  team: string;
+  positions: string[];
+  valueScore: number;
+};
